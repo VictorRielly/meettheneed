abee3b7f3bf5e37852a338a83adc831fa2e960ae
Brianna Alcoran <alco8653@pacificu.edu>
2016-04-16 22:40:24 -0700

    made a lot of changes

---
+++ app/src/main/java/com/search/victor/searchengine/CustomAutoCompleteView.java
@@ -0,0 +1,8 @@
+package com.search.victor.searchengine;
+
+/**
+ * Created by alcoranb on 4/14/16.
+ */
+public class CustomAutoCompleteView
+{
+}
---
+++ app/src/main/java/com/search/victor/searchengine/utils/Constants.java
@@ -0,0 +1,8 @@
+package com.search.victor.searchengine.utils;
+
+/**
+ * Created by alcoranb on 4/13/16.
+ */
+public class Constants
+{
+}
